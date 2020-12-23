54cc8158ce024d051cd7f97bee9d182459996b75
winpasit <42579006+winpasit@users.noreply.github.com>
2020-12-24 02:42:09 +0700

    add api for get papers

--- esra_backend/urls.py
+++ esra_backend/urls.py
@@ -7,8 +7,13 @@ urlpatterns = [
         include('rest_framework.urls',namespace='rest_framework')
     ),
     path(
-        'paper/',
+        'paper/get_paper',
         GetPaper.as_view(),
         name='get_paper'
+    ),
+    path(
+        'paper/get_papers',
+        GetPapers.as_view(),
+        name='get_papers'
     ),
 ]
--- esra_backend/views.py
+++ esra_backend/views.py
@@ -53,10 +53,67 @@ class GetPaper(generics.RetrieveAPIView):
 
     def get(self, request, *args, **kargs):
         try:
-            paper_obj = self.get_paper()
+            obj = self.get_paper()
         except:
             return Response("Paper doesn't not exist.",status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        return Response(paper_obj, status=status.HTTP_200_OK)
+        return Response(obj, status=status.HTTP_200_OK)
+
+class GetPapers(generics.RetrieveAPIView):
+    def get_paper(self,paper_id):
+        result_object = {}
+        paper = Paper.objects.get(pk=paper_id)
+
+        #handle author
+        writes = WriteEntity.objects.filter(paper_id=paper_id) #list of writer object
+        authors = []
+        for write in writes:
+            authors.append(Author.objects.get(author_id=write.author_id)) #list of authors
+
+        #handle affiliation
+        affiliations = []
+        for author in authors:
+            temp_affs = Affiliation.objects.filter(author_id=author.author_id)
+            for temp_aff in temp_affs:
+                affiliations.append(temp_aff)
+
+        # create return object
+        result_object['paper_id'] = paper_id
+        result_object['paper_title'] = paper.paper_title
+        result_object['conference'] = paper.conference
+        result_object['arxiv_id'] = paper.arxiv_id
+        result_object['mag_id'] = paper.mag_id
+        result_object['citation_count'] = paper.citation_count
+        result_object['publish_date'] = paper.publish_date
+        
+        result_authors = []
+        for author in authors:
+            temp_author = {}
+            temp_author['author_name'] = author.author_name
+            temp_aff = []
+            for affiliation in affiliations:
+                if affiliation.author_id == author.author_id:
+                    temp_aff.append(affiliation.affiliation_name)
+            temp_author['affiliations'] = temp_aff
+            result_authors.append(temp_author)
+        result_object['authors'] = result_authors
+        return result_object
+
+    def get_papers(self):
+        list_of_paper_id = self.request.data['paper_ids']
+        list_of_result = []
+        for paper_id in list_of_paper_id:
+            paper = self.get_paper(paper_id)
+            list_of_result.append(paper)
+        return { 'papers':list_of_result}
+            
+
+    def get(self, request, *args, **kargs):
+        try:
+            obj = self.get_papers()
+        except:
+            return Response("Some papers doesn't not exist.",status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(obj, status=status.HTTP_200_OK)
+
 
         
 
