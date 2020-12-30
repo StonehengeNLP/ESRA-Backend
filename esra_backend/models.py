dacc654f3f6c250e2469919abc7c31ab49a7317c
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2020-12-30 14:48:54 +0700

    Change: database models

--- esra_backend/models.py
+++ esra_backend/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Paper(models.Model):
     paper_id = models.AutoField(primary_key=True, null=False)
     paper_title = models.CharField(max_length=512, null=False)
@@ -11,13 +10,17 @@ class Paper(models.Model):
     publish_date = models.DateField(null=False)
     abstract = models.TextField(null=True)
     cite_to = models.ManyToManyField('self', symmetrical=False, null=True, blank=True)
+    jornal_name = models.TextField(null=True)
 
 class Author(models.Model):
     author_id = models.AutoField(primary_key=True, null=False)
     author_name = models.CharField(max_length=512, null=False)
-    paper = models.ManyToManyField(Paper)
     
 class Affiliation(models.Model):
     affiliation_id = models.AutoField(primary_key=True, null=False)
     affiliation_name = models.CharField(max_length=256, null=False)
-    author = models.ManyToManyField(Author)
+
+class PaperAuthorAffiliation(models.Model):
+    paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    affiliation = models.ForeignKey(Affiliation, on_delete=models.CASCADE)
