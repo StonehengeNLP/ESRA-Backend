1961ae908f4d822df2b7cc2ce96f6f48e4a80def
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2021-02-02 01:07:06 +0700

    Fix: debug=False, set allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG_MODE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
