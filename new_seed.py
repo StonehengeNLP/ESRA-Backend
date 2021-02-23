0da8d1ef48e449f5d71a63529f1ff659ac2464fe
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2021-02-23 23:16:25 +0700

    Add: paper authorr relation

--- new_seed.py
+++ new_seed.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import datetime, requests, json
 import os
 from datetime import date
@@ -32,10 +33,28 @@ def add_entries(df):
                 paper=p, author=a, affiliation=None
             )
     print('finish adding entries!')
-            
+
+def add_cite(df):
+    print('start adding citation!')
+    # use reference field
+    for i, row in tqdm(df.iterrows(), total=len(df)):
+        ref = row.references
+        if not isinstance(ref, list) and ref != 0:
+            ref = eval(ref)
+        if ref == None or ref == [] or ref == 0: 
+            continue
+        ref_papers = Paper.objects.filter(arxiv_id__in=ref)
+        og_paper = Paper.objects.get(arxiv_id=row.arxiv_id)
+        og_paper.cite_to.add(*ref_papers)
+    print('finish adding citation!')
+
+
 if __name__ == '__main__':
     # csv
     df = pd.read_csv('merged.csv')
     df.cc = df.cc.fillna(0)
     df.rc = df.rc.fillna(0)
-    add_entries(df)
+    df.references = df.references.fillna(0)
+    df.citations = df.citations.fillna(0)
+    # add_entries(df)
+    add_cite(df)
